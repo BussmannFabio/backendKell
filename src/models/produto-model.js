35b9a41033bd01f5b5e2cf11289a42b17aea454e
Fabio <fabio12513@gmail.com>
2026-04-13 07:41:13 -0300

    MudancaNosRelatórios

--- src/models/produto-model.js
+++ src/models/produto-model.js
@@ -6,10 +6,10 @@ const Produto = sequelize.define('Produto', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   codigo: { type: DataTypes.STRING(50), allowNull: false, unique: true },
   descricao: { type: DataTypes.STRING(255), allowNull: true },
-  valorMaoDeObraDuzia: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-  valorMaoDeObraPeca: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-  precoVendaDuzia: { type: DataTypes.DECIMAL(12, 2), allowNull: true, defaultValue: 0.00 },
-  precoVendaPeca: { type: DataTypes.DECIMAL(12, 2), allowNull: true, defaultValue: 0.00 }
+  valorMaoDeObraDuzia: { type: DataTypes.DECIMAL(10, 4), allowNull: false },
+  valorMaoDeObraPeca: { type: DataTypes.DECIMAL(10, 4), allowNull: false },
+  precoVendaDuzia: { type: DataTypes.DECIMAL(12, 4), allowNull: true, defaultValue: 0.00 },
+  precoVendaPeca: { type: DataTypes.DECIMAL(12, 4), allowNull: true, defaultValue: 0.00 }
 }, {
   tableName: 'produtos',
   timestamps: false,
